66dc8054eaed4228a0cc76c75feb00e26496da07
Geghamy <102752680+Geghamy@users.noreply.github.com>
2022-04-04 18:00:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const btn = document.getElementById('btn');
-const wall = document.getElementById('wall');
-const color = document.getElementById('color');
+const body = document.getElementsByTagName('body');
 
 const colors = ['red', '#FFA500', 'rgb(255,255,0)', 'purple'];
-color.innerHTML = colors[1];
- let index = 0;
+color.innerHTML = colors[0];
+let index = 0;
+document.body.style.backgroundColor = colors[0];
 
 btn.addEventListener('click', function onClick() {
-  wall.style.backgroundColor = colors[index];
-  color.innerHTML = colors[index];
+
+  document.body.style.backgroundColor = colors[index+1];
+  color.innerHTML = colors[index+1];
   
-  index = index >= colors.length - 1 ? 0 : index + 1;
+  index = index >= colors.length - 2 ? 0 : index + 1;
 });
